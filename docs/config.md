ac50b00b3ba22ffa5a74dbee3347158041ef2e86
Emilio <emilioplatzer@gmail.com>
2015-10-20 12:35:38 -0300

    Preparando el repositorio.

---
+++ docs/config.md
@@ -0,0 +1,11 @@
+# configuración
+
+```js
+qacService.init(config);
+```
+
+## campos de config:
+
+parametro  | uso
+-----------|--------------------
+repository | ruta al repositorio
---
+++ test/test-resume.js
@@ -0,0 +1,51 @@
+"use strict";
+/*jshint eqnull:true */
+/*jshint globalstrict:true */
+/*jshint node:true */
+/* global describe */
+/* global it */
+
+var expect = require('expect.js');
+var sinon = require('sinon');
+var Promises = require('best-promise');
+var qacServices = require('../lib/qac-services.js');
+var fs = require('fs-promise');
+
+var request = require('supertest');
+
+qacServices.config({
+    repository:'test/fixtures/repo4display'
+});
+
+describe("qac-services overview",function(){
+    it("make the overview",function(done){
+        qacServices.makeOverviewMd('sourcetravelers').then(function(contentMd){
+            return fs.readFile('test/fixtures/repo4display/expected/resume-sourcetravelers.md', {encoding:'utf8'})
+            .then(function(expectedContent){
+                expect(contentMd).to.be(expectedContent);
+            });
+        }).then(done,done);
+    });
+    it("receive one push",function(done){
+        var htmlContent = "random 123123123123 html content";
+        var mock = sinon.stub(qacServices, 'makeOverviewHtml');
+        mock.withArgs('sourcetravelers').returns(Promises.resolve(htmlContent));
+        var server = createServer();
+        var agent=request(server);
+        agent
+        .get('/sourcetravelers')
+        .expect(htmlContent)
+        .end(done);
+    });
+});
+
+var express = require('express');
+
+function createServer(opts, fn) {
+    qacServices.config(opts);
+    var _serve = qacServices.overviewServe();
+    var app = express();
+    app.listen();
+    app.use(_serve);
+    return app;
+}
