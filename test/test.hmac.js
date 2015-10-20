5333a9553182c5893f7d1ce6a8bc98dd79e6aa30
Diego F.(i7) <diegoefe@gmail.com>
2015-10-20 12:23:26 -0300

    Agrego test para validar request de github

--- test/test.hmac.js
+++ test/test.hmac.js
@@ -3,12 +3,12 @@
 var expect = require('expect.js');
 var Promises = require('best-promise');
 var fs = require('fs-promise');
+var qacServices = require('../lib/qac-services.js');
 
 describe('HMac', function(){
-    it('payload test', function(done){
+    it('payload should validate with secret key', function(done){
         fs.readFile('./test/hmac.payload', {encoding: 'utf8'}).then(function(payload) {
-            //console.log("payload", payload);
-            //expect()'sha1=49ee18e35e373f08f6984e5e40f885030e81105b'
+            expect(qacServices.isValidRequest(payload, 'sha1=49ee18e35e373f08f6984e5e40f885030e81105b', 'elsecreto')).to.be.ok();
             done();
         }).catch(function(err) {
             console.log("mal", err);
