0bed4b740c8fb0a11d63d9398611636de1bb3c7f
Diego F <diegoefe@gmail.com>
2015-11-23 18:37:15 -0300

    Nuevo esquema para testear

--- test/test-resume.js
+++ test/test-resume.js
@@ -25,6 +25,76 @@ describe("qac-services overview",function(){
             expect(content).to.be(expectedContent);
         }).then(done,done);
     });
+	it.skip("get cucardasToHtmlList",function(){
+		var obt=qacServices.cucardasToHtmlList(
+			"[![npm-version](https://img.shields.io/npm/v/multilang.svg)](https://npmjs.org/package/multilang)  \n\r "+
+			"[![downloads](https://img.shields.io/npm/dm/multilang.svg)](https://npmjs.org/package/multilangx)"
+		);
+		expect(obt).to.eql([
+			html.a(
+				{href:"https://npmjs.org/package/multilang"}, 
+				[ html.img({src:"https://img.shields.io/npm/v/multilang.svg", title:"npm-version"}) ]
+			),
+			html.a(
+				{href:"https://npmjs.org/package/multilangx"}, 
+				[ html.img({src:"https://img.shields.io/npm/dm/multilang.svg", title:"downloads"}) ]
+			)
+		]);
+	});
+	it.skip("get projectNameToHtmlLink",function(){
+		qacServices.rootUrl='/root/'
+		var obt=qacServices.projectNameToHtmlLink('simple-org','proj-name');
+		expect(obt).to.eql(
+			html.a({href:"/root/simple-org/proj-name"}, "proj-name")
+		);
+	});
+    it.skip('getOrganization simple organization', function(done) {
+		sinon.stub(qacServices, "getInfo", function(organization, project){
+			if(!!project){
+				throw new Error("unexpected project name in getInfo");
+			}
+			if(organization!=='simple'){
+				throw new Error("wrong organization name in getInfo");
+			}
+			return {
+				organization:{
+					projects:[{
+						projectName:'uno'
+					},{
+						projectName:'dos'
+					}],
+					path:'the-org-path'
+				}
+			};
+		});
+		sinon.stub(fs, 'readFile', function(nameCucardas){
+			switch(nameCucardas){
+			case 'the-org-path/projects/uno/result/cucardas.md': return 'cu-uno';
+			case 'the-org-path/projects/dos/result/cucardas.md': return 'cu-dos';
+			default: throw new Error('unexpected params in readFile of cucardas');
+			}
+		});
+		sinon.stub(qacServices, "cucardasToHtmlList", function(x){
+			return ["list: "+x];
+		});
+		sinon.stub(qacServices, "projectNameToHtmlLink", function(orga, proj){
+			return "link: "+orga+','+proj;
+		});
+        qacServices.getOrganizationPage('simple').then(function(oHtml){
+            expect(oHtml).to.be(
+				html.table([
+					html.tr([ html.th('project'), html.th('cucardas') ]),
+					html.tr([ html.td("link: simple,uno"), html.td( ["list: cu-uno"]) ]),
+					html.tr([ html.td("link: simple,dos"), html.td( ["list: cu-dos"]) ]),
+				])
+			);
+		}).then(function(){
+			qacServices.getInfo.restore();
+			qacServices.cucardasToHtmlList.restore();
+			qacServices.projectNameToHtmlLink.restore();
+			fs.readFile.restore();
+        }).then(done,done);
+    });
     it("obtain html",function(done){
         var htmlContent = "random 123123123123 html content";
         var mock = sinon.stub(qacServices, 'makeOverviewHtml');
